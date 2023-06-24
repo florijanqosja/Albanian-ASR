ab6ea6efe7257b9bd1be32ba27247f3b861ae75b
florijanqosja <fqo@levelft.com>
2023-06-24 18:54:52 +0100

    updated.env

--- api/database/database.py
+++ api/database/database.py
@@ -1,11 +1,27 @@
+import os
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+from dotenv import dotenv_values
 
-DATABASE_URL = "postgresql://your_username:your_password@db/your_database_name"
+# Load environment variables from .env file
+env_vars = dotenv_values()
 
+# Retrieve the database credentials from environment variables
+DATABASE_USER = env_vars.get("POSTGRES_USER")
+DATABASE_PASSWORD = env_vars.get("POSTGRES_PASSWORD")
+DATABASE_NAME = env_vars.get("POSTGRES_DB")
+DATABASE_HOST = env_vars.get("DATABASE_HOST")
+DATABASE_PORT = env_vars.get("DATABASE_PORT")
+
+# Create the database connection URL
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+# Create the database engine
 engine = _sql.create_engine(DATABASE_URL)
 
+# Create a session factory
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create a base class for declarative models
 Base = _declarative.declarative_base()
