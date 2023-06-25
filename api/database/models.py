c157fb0dcf3547ac10f8ca1118953c5eee045b0b
florijanqosja <fqo@levelft.com>
2023-06-25 12:50:09 +0100

    small fixes

--- api/database/models.py
+++ api/database/models.py
@@ -4,14 +4,6 @@ import sqlalchemy as _sql
 from . import database as _database
 
 
-class Contact(_database.Base):
-    __tablename__ = "contacts"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    first_name = _sql.Column(_sql.String, index=True)
-    last_name = _sql.Column(_sql.String, index=True)
-    email = _sql.Column(_sql.String, index=True, unique=True)
-    phone_number = _sql.Column(_sql.String, index=True, unique=True)
-    date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
 
 class Video_table(_database.Base):
     __tablename__ = "video_table"
@@ -56,6 +48,16 @@ class High_quality_labeled_splice_table(_database.Base):
 
 class Deleted_splice_table(_database.Base):
     __tablename__ = "deleted_splice_table"
+    Sp_ID = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    Sp_NAME = _sql.Column(_sql.String, nullable=True)
+    Sp_PATH = _sql.Column(_sql.String, nullable=True)
+    Sp_LABEL = _sql.Column(_sql.String, nullable=True)
+    Sp_ORIGIN = _sql.Column(_sql.String, nullable=True)
+    Sp_DURATION = _sql.Column(_sql.String, nullable=True)
+    Sp_VALIDATION = _sql.Column(_sql.String, nullable=True)
+
+class Splice_beeing_processed_table(_database.Base):
+    __tablename__ = "splice_beeing_processed_table"
     Sp_ID = _sql.Column(_sql.Integer, primary_key=True, index=True)
     Sp_NAME = _sql.Column(_sql.String, nullable=True)
     Sp_PATH = _sql.Column(_sql.String, nullable=True)
